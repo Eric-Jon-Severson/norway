fab7d72d908dc8f53f0d10fc4520ac3b26c82360
Eric-Jon-Severson <severson.eric23@gmail.com>
2020-05-18 12:43:11 -0500

    first commit

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\muski\Desktop\vs_code\plotly-dash-django-udemy-master\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
